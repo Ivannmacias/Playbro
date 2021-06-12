e7a23a2a923de1a3d12dce889ab6d4567005e48a
Ivann <ivann.macias@gmail.com>
2021-06-12 14:00:24 -0500

    Se agrego nueva rama

--- ivann.js
+++ ivann.js
@@ -1,11 +1,7 @@
 let nombre = "Ivan Macias"
 
-<<<<<<< HEAD
-const arreglo = ["cucei","cucea","prepa13"]
-=======
 const arreglo = ["cucei","cucea","conalep"]
->>>>>>> 18536d11f3d9395c311aa16566688f0458a15d9f
 
-const arreglo2 = ["ciencias","matematicas","historia"]
+const arreglo2 = ["ciencias","matematicas"]
 
 
