0816565a991414137f90405afaa0b953804e11fc
Ivann <ivann.macias@gmail.com>
2021-06-12 14:38:12 -0500

    Se modifica

--- ivann.js
+++ ivann.js
@@ -1,10 +1,6 @@
 let nombre = "Ivan Macias"
 
-<<<<<<< HEAD
-const arreglo = ["cucei","cucea","prepa13"]
-=======
 const arreglo = ["cucei","cucea","conalep"]
->>>>>>> 18536d11f3d9395c311aa16566688f0458a15d9f
 
 const arreglo2 = ["ciencias","matematicas","historia"]
 
